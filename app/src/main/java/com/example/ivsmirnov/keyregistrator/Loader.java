20dce87323e2310029219cdcd8574986ee074f5f
jkstop <android.jkstop@gmail.com>
2015-06-29 13:28:22 +0300

    Key Registrator First GIT 29.06.15

--- app/src/main/java/com/example/ivsmirnov/keyregistrator/Loader.java
+++ app/src/main/java/com/example/ivsmirnov/keyregistrator/Loader.java
@@ -26,7 +26,7 @@ public class Loader extends AsyncTask <String,Void,Void> {
         fragmentManager = fm;
     }
 
-//chg
+//chggdfg
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
